97d6345a44d89ba1e09d9034ddcdf1e4416a8909
NestiUtari <nestiutari03@gmail.com>
2023-09-10 00:47:49 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+   $server = "localhost";
+   $username = "root";
+   $password = "";
+   $database = "pmb_nestiutari_uas";
+
+   $conn = mysqli_connect($server,$username,$password,$database);
+?>
